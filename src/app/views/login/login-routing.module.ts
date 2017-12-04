696da3617fad7e3851f10cdbf41a6b1f9ef93a0f
Giovanni Brigoli <giovanni.brigoli@nokia.com>
2017-12-04 11:22:10 +0800

    modularize app

---
+++ src/app/views/login/login-routing.module.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
+
+export const LoginRoutingModule: ModuleWithProviders = RouterModule.forChild(routes);
