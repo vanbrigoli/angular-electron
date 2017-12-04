f63d2e6d77dd0892f34cff56fb610c2a123b5343
Giovanni Brigoli <giovanni.brigoli@nokia.com>
2017-12-04 16:29:50 +0800

    add core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 
 import { AuthService } from './index';
 
 @NgModule({
   providers: [AuthService]
 })
-export class CoreModule { }
+export class CoreModule {
+  // Prevent importing core module
+  constructor(@Optional() @SkipSelf() core: CoreModule) {
+    if(core) throw new Error("Thou shall not run.");
+  }
+}
